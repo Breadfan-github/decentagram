c9ef6682da55612e7c865d27008344674eb53876
Hin <106875651+HinNFT@users.noreply.github.com>
2022-12-25 02:34:14 +0800

    Add files via upload

---
+++ build/precache-manifest.adf64d7b62806caf855cb49c50b3f2d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0747244f426dc0502040571966ff7d99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebdd630242f16c89a2bc",
+    "url": "./static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "8bd5d44498ea93922f74",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "ebdd630242f16c89a2bc",
+    "url": "./static/js/2.a812551c.chunk.js"
+  },
+  {
+    "revision": "efe28990faa207b313ac9f2132670f72",
+    "url": "./static/js/2.a812551c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd5d44498ea93922f74",
+    "url": "./static/js/main.9ad58b70.chunk.js"
+  },
+  {
+    "revision": "35eff47ceeca097ad7eb",
+    "url": "./static/js/runtime-main.2bd40bad.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "./static/media/photo.d6cbd8e6.png"
+  }
+]);
